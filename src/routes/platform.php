7b7fd5ff8dd4430634718acc7ce3986a5e7bd1f3
Oksana <art.oksana18@gmail.com>
2025-12-27 04:08:22 +0400

    закомментирована ошибка в роутах orchid

--- src/routes/platform.php
+++ src/routes/platform.php
@@ -10,25 +10,25 @@ use Orchid\Platform\Screens\User\UserEditScreen;
 use Orchid\Platform\Screens\User\UserListScreen;
 use Orchid\Platform\Screens\User\UserProfileScreen;
 
-Route::middleware((array) config('platform.middleware.private'))
-    ->domain(config('platform.domain'))
-    ->prefix((string) config('platform.prefix', 'admin'))
-    ->group(function () {
-        Route::screen('main', DashboardScreen::class)
-            ->name('platform.main');
+// Route::middleware((array) config('platform.middleware.private'))
+//     ->domain(config('platform.domain'))
+//     ->prefix((string) config('platform.prefix', 'admin'))
+//     ->group(function () {
+//         Route::screen('main', DashboardScreen::class)
+//             ->name('platform.main');
 
-        Route::screen('profile', UserProfileScreen::class)
-            ->name('platform.profile');
+//         Route::screen('profile', UserProfileScreen::class)
+//             ->name('platform.profile');
 
-        Route::screen('users/{user}/edit', UserEditScreen::class)
-            ->name('platform.systems.users.edit');
+//         Route::screen('users/{user}/edit', UserEditScreen::class)
+//             ->name('platform.systems.users.edit');
 
-        Route::screen('users', UserListScreen::class)
-            ->name('platform.systems.users');
+//         Route::screen('users', UserListScreen::class)
+//             ->name('platform.systems.users');
 
-        Route::screen('roles/{role}/edit', RoleEditScreen::class)
-            ->name('platform.systems.roles.edit');
+//         Route::screen('roles/{role}/edit', RoleEditScreen::class)
+//             ->name('platform.systems.roles.edit');
 
-        Route::screen('roles', RoleListScreen::class)
-            ->name('platform.systems.roles');
-    });
+//         Route::screen('roles', RoleListScreen::class)
+//             ->name('platform.systems.roles');
+//     });
